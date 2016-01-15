1639f695c177a6ba61539b335796a3b87283d599
Ryan Newton <rnewton@hearsaycorp.com>
2016-01-14 17:09:55 -0800

    Location and Timer working

---
+++ app/src/main/java/com/fadetoproductions/rvkn/locationtracker/MainActivity.java
@@ -0,0 +1,143 @@
+package com.fadetoproductions.rvkn.locationtracker;
+
+import com.google.android.gms.location.LocationListener;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+
+    static Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeGoogleApiClient();
+        startSendingLocationAtInterval();
+
+    }
+
+    public void startSendingLocationAtInterval() {
+
+
+        timer = new Timer();
+        timer.schedule(new SendLocationTimerTask(), 0, 5000);
+
+    }
+
+    protected void initializeGoogleApiClient() {
+        // Create the location client to start receiving updates
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d("DEBUG", "STARTED");
+        // Connect the client.
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+//        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    public void onConnected(Bundle dataBundle) {
+        Log.d("DEBUG", "CONNECTED");
+        // Get last known recent location.
+        Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        // Note that this can be NULL if last location isn't already known.
+        if (mCurrentLocation != null) {
+            // Print current location if not null
+            Log.d("DEBUG", "current location: " + mCurrentLocation.toString());
+            LatLng latLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        }
+        else {
+            Log.d("DEBUG", "Current Location is NULL");
+        }
+//         Begin polling for new location updates.
+
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("DEBUG", "SUSPENDED");
+
+
+        if (i == CAUSE_SERVICE_DISCONNECTED) {
+            Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+        } else if (i == CAUSE_NETWORK_LOST) {
+            Toast.makeText(this, "Network lost. Please re-connect.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("DEBUG", "FAILED");
+    }
+
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        // Request location updates
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+
+    public class SendLocationTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            Log.d("DEBUG", "TIMER TASK RUN");
+            Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mCurrentLocation != null) {
+                // Print current location if not null
+                Log.d("DEBUG", "current location: " + mCurrentLocation.toString());
+                LatLng latLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+            }
+        }
+    }
+}
